7522a7520f758c043c56225009b578530e6ef167
Forsen02 <129263203+a22ricni@users.noreply.github.com>
2023-05-03 09:58:46 +0200

    Mountain class with attributes
    I made a mountain class so that the I can use the attributes from the json.

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,49 @@
+package com.example.networking;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Mountain {
+
+    private String ID;
+    private String name;
+    @SerializedName("size") private int height;
+
+    public Mountain(String ID, String name, int height) {
+        this.ID = ID;
+        this.name = name;
+        this.height = height;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Mountain{" +
+                "ID='" + ID + '\'' +
+                ", name='" + name + '\'' +
+                ", height=" + height +
+                '}';
+    }
+}
